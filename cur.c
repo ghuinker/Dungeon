a93cd638c1493cfbc76173b4d471e19926a6bc27
Gabe Huinker <ghuinker@gmail.com>
2018-10-17 00:27:32 -0500

    Adding Monster menu

--- cur.c
+++ cur.c
@@ -40,6 +40,8 @@ void render_dungeon_curs(dungeon_t *d){
   refresh();
 }
 
+
+
 void print_message(char *mes){
   int i=0;
   while(mes[i]){
@@ -50,6 +52,62 @@ void print_message(char *mes){
 void clear_message(){
   move(0, 0);
   clrtoeol();
+}
+
+
+
+void mon_menu(dungeon_t *d){
+  clear_message();
+
+
+  pair_t p;
+  
+  char *menu = "Monster Menu";
+
+  char *list = malloc(34 * d->num_monsters);
+
+  int size = 0;
+
+  endwin();
+  for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) 
+    for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++)
+      if (charpair(p)) 
+	if(charpair(p)->symbol != '@'){
+	  memcpy(list+size, "Monster ", 8);
+	  size = size +8;
+	  memcpy(list + size, &charpair(p)->symbol, 1);
+	  size = size +1;
+	  memcpy(list+size, " : ", 3);
+	  size = size +3;
+	  
+	  int dist_y = charpair(p)->position[dim_y] - d->pc.position[dim_y];
+	  int dist_x = charpair(p)->position[dim_x] - d->pc.position[dim_x];
+
+	  char loc[22];
+
+	  char vert[6];
+	  char hor[5];
+
+	  if(dist_y < 0)
+	    strcpy(vert, "South");
+	  else
+	    strcpy(vert, "North");
+	  if(dist_x < 0)
+	    strcpy(hor, "West");
+	  else
+	    strcpy(hor, "East");
+	
+	  sprintf(loc, "%d %s and %d %s\n", abs(dist_y), vert, abs(dist_x), hor);
+	  memcpy(list+size, loc, 22);
+	  size = size + 22;
+	}
+    
+  printf("%s\n", menu);
+  printf("%s\n", list);
+
+  free(list);
+  exit(1);
+  clear();
 }
 
 void run_curses(dungeon_t *d){
@@ -138,6 +196,8 @@ void run_curses(dungeon_t *d){
 
 
 void init_curses(dungeon_t *d){
+
+  mon_menu(d);
   initscr();
   raw();
   noecho();
@@ -146,7 +206,5 @@ void init_curses(dungeon_t *d){
   
   
   render_dungeon_curs(d);
-
-  
   
 }
